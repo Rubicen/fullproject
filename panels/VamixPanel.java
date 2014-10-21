2cd11cac63b49d0ecded998f28a4373cff82f35f
kokobrainz <logan.horton@live.com>
2014-10-22 11:27:10 +1300

    moved into packages and fixed up general blemishes in UI

---
+++ panels/VamixPanel.java
@@ -0,0 +1,36 @@
+package panels;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JPanel;
+
+
+public abstract class VamixPanel extends JPanel{
+
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Abstract method that will be used to tell panel what to do when a
+	 * file is opened in the OpenPanel
+	 */
+	abstract void newInput(File file,Boolean boo);
+	
+	/**
+	 * Helper method that starts the given command in bash
+	 * @param cmd the command to be executed
+	 */
+	protected int bashCommand(String cmd){
+		ProcessBuilder builder = new ProcessBuilder("/bin/bash", "-c", cmd);
+		
+		try {
+			Process process = builder.start();
+			return process.waitFor();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return 1;
+	}
+}
